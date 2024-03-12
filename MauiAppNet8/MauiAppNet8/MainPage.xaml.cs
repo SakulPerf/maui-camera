a8300745a13b49d84e9acdca5465fd8a2cbc2df0
Au <au.perspectives@gmail.com>
2024-03-12 15:55:46 +0700

    Smoother capture but not enough.

--- MauiAppNet8/MauiAppNet8/MainPage.xaml.cs
+++ MauiAppNet8/MauiAppNet8/MainPage.xaml.cs
@@ -2,18 +2,47 @@
 
 public partial class MainPage : ContentPage
 {
+    private volatile static Queue<Image> Images = [];
+
     public MainPage()
     {
         InitializeComponent();
+        var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(300));
         cameraView.CamerasLoaded += (sdnr, se) =>
         {
             MainThread.BeginInvokeOnMainThread(async () =>
             {
                 cameraView.Camera = cameraView.Cameras.First();
                 await cameraView.StopCameraAsync();
-                await cameraView.StartCameraAsync();
+                var smallest = cameraView.Camera.AvailableResolutions.MinBy(it => it.Width * it.Height);
+                await cameraView.StartCameraAsync(smallest);
             });
         };
-        CaptureButton.Clicked += (sender, e) => ImagePreview.Source = cameraView.GetSnapShot();
+        CaptureButton.Clicked += (sender, e) => Capture();
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            while (await timer.WaitForNextTickAsync())
+            {
+                if (AutoCapture.IsChecked) Capture();
+                while (Images.TryDequeue(out var img))
+                {
+                    Container.Add(img);
+                }
+            }
+        });
+    }
+
+    private void Capture()
+    {
+        var thread = new Thread(new ThreadStart(() =>
+        {
+            Images.Enqueue(new Image
+            {
+                WidthRequest = 60,
+                HeightRequest = 60,
+                Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.JPEG),
+            });
+        }));
+        thread.Start();
     }
 }
